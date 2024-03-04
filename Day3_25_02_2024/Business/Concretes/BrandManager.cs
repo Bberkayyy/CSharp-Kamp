a65592f77eb077bf493fa0ef3482678e69b10c8b
Bberkayyy <bberkayoguz@gmail.com>
2024-03-04 22:08:13 +0300

    Camp Day3

---
+++ Day3_25_02_2024/Business/Concretes/BrandManager.cs
@@ -0,0 +1,56 @@
+﻿using Business.Abstracts;
+using Business.Dtos.Requests;
+using Business.Dtos.Responses;
+using DataAccess.Abstracts;
+using Entities.Concretes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concretes;
+
+public class BrandManager : IBrandService
+{
+    private readonly IBrandDal _brandDal;
+
+    public BrandManager(IBrandDal brandDal)
+    {
+        _brandDal = brandDal;
+    }
+
+    public CreatedBrandResponse TAdd(CreateBrandRequest createBrandRequest)
+    {
+        //add rules
+
+        Brand brand = new();
+        brand.Name = createBrandRequest.Name;
+        brand.CreatedDate = DateTime.Now;
+
+        _brandDal.Add(brand);
+
+        CreatedBrandResponse createdBrandResponse = new CreatedBrandResponse();
+        createdBrandResponse.Id = 4;
+        createdBrandResponse.Name = createBrandRequest.Name;
+        createdBrandResponse.CreatedDate = brand.CreatedDate;
+
+        return createdBrandResponse;
+    }
+
+    public List<GetAllBrandResponse> TGetAll()
+    {
+        List<Brand> brands = _brandDal.GetAll();
+        List<GetAllBrandResponse> brandResponse = new List<GetAllBrandResponse>();
+        foreach (var brand in brands)
+        {
+            GetAllBrandResponse getAllBrandResponse = new GetAllBrandResponse();
+            getAllBrandResponse.Name = brand.Name;
+            getAllBrandResponse.Id = brand.Id;
+            getAllBrandResponse.CreatedDate = brand.CreatedDate;
+
+            brandResponse.Add(getAllBrandResponse);
+        }
+        return brandResponse;
+    }
+}
